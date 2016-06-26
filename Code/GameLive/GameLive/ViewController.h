c267790b096dfb8ab9920f4111ed2852bc46a817
Asdkuam <3167265663@qq.com>
2016-06-26 14:44:47 +0800

    游戏直播项目搭建
    测试上传

---
+++ Code/GameLive/GameLive/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameLive
+//
+//  Created by tarena on 16/6/26.
+//  Copyright © 2016年 Tedu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
